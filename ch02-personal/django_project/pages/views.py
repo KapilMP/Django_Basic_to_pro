37e5706e01081d36eb78125409d0955faaff7f38
KapilMP <kapilpradhan77ik@gmail.com>
2025-03-05 11:34:57 +0545

    Deleting files

--- ch02-personal/django_project/pages/views.py
+++
@@ -1,8 +0,0 @@
-from django.views.generic import TemplateView
-
-class homePageView(TemplateView):
-    template_name = "home.html"
-
-class aboutPageView(TemplateView):
-    template_name = "about.html"
-
