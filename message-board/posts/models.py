853add65a5150eb201a9c41829e8a6cfa5ab30b7
KapilMP <kapilpradhan77ik@gmail.com>
2025-03-05 13:01:32 +0545

    New folder that containes superapps

---
+++ message-board/posts/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+
+class Posts(models.Model):
+    text = models.TextField()
+
+    def __str__(self):
+        return self.text[:50]
+
+# Create your models here.
---
+++ message-board/posts/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+from django.views.generic import ListView
+from .models import Posts
+
+class HomePageView(ListView):
+    model = Posts
+    template_name = "home.html"
+
+
+# Create your views here.
