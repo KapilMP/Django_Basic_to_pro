e0a07ae2aab5649f59cad68253aec501a0d3dad2
KapilMP <kapilpradhan77ik@gmail.com>
2025-03-05 17:01:22 +0545

    Added basic model for understanding

--- basic/pages/views.py
+++ basic/pages/views.py
@@ -1,13 +1,25 @@
 from django.shortcuts import render
-from django.views.generic import TemplateView
+# from django.views.generic import TemplateView
+from .models import feature
 
 def HomePageView(request):
+    feature1 = feature()
+    feature1.id = 1
+    feature1.name = "Harry"
+    feature1.details ="We are quick"
 
-    return render(request, "home.html")
+    feature2 = feature()
+    feature2.id = 2
+    feature2.name = "Prakash"
+    feature2.details ="We are quicker"
+
+    features = [feature1, feature2]
+
+    return render(request, "home.html", {'feature': features})
 
 def CountPageView(request):
-    words = request.GET['counter']
-    number_of_words = len(words.split())
+    words = request.POST['counter']
+    number_of_words = len(words.split()) if words else 0
     return render(request, "counter.html", {'count': number_of_words})
 
 
